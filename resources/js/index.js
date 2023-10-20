8500ee4fb3af36a66d57386e19ef51b3845b3fa4
Camilo Andres Munoz <camu.ozc1@gmail.com>
2023-10-20 15:01:50 -0500

    commit cambios en estructura proyecto de practica git

--- resources/js/index.js
+++ resources/js/index.js
@@ -5,6 +5,7 @@ const cardIdCheckout = document.getElementById("cardgitCheckout");
 const cardIdTag = document.getElementById("cardgitTag");
 const cardIdShow = document.getElementById("cardgitShow");
 const cardIdLog = document.getElementById("cardgitLog");
+const cardIdStatus = document.getElementById("cardgitStatus");
 const cardIdFetch = document.getElementById("cardgitFetch");
 const cardIdPull = document.getElementById("cardgitPull");
 const cardIdPush = document.getElementById("cardgitPush");
@@ -12,6 +13,15 @@ const cardIdMerge = document.getElementById("cardgitMerge");
 const cardIdReset = document.getElementById("cardgitReset");
 const cardIdSSHW = document.getElementById("cardgitSSHW");
 const cardIdSSHL = document.getElementById("cardgitSSHL");
+const cardIdConfig = document.getElementById("cardgitConfig");
+const cardIdRemoteAdd = document.getElementById("cardgitRemoteAdd");
+const cardIdRemoteRemove = document.getElementById("cardgitRemoteRemove");
+const cardIdRebase = document.getElementById("cardgitRebase");
+const cardIdRevert = document.getElementById("cardgitRevert");
+const cardIdDiff = document.getElementById("cardgitDiff");
+const cardIdStash = document.getElementById("cardgitStash");
+const cardIdCommit = document.getElementById("cardgitCommit");
+const cardIdAdd = document.getElementById("cardgitAdd");
 const git = document.getElementById("git");
 const welcome = document.getElementById("welcome");
 
@@ -23,6 +33,7 @@ const listCards = [
   cardIdTag,
   cardIdShow,
   cardIdLog,
+  cardIdStatus,
   cardIdFetch,
   cardIdPull,
   cardIdPush,
@@ -30,6 +41,15 @@ const listCards = [
   cardIdReset,
   cardIdSSHW,
   cardIdSSHL,
+  cardIdConfig,
+  cardIdRemoteAdd,
+  cardIdRemoteRemove,
+  cardIdRebase,
+  cardIdRevert,
+  cardIdDiff,
+  cardIdStash,
+  cardIdCommit,
+  cardIdAdd,
   git,
   welcome
 ];
@@ -53,6 +73,7 @@ selectTema.addEventListener("change", () => {
     "git tag": "cardgitTag",
     "git show": "cardgitShow",
     "git log": "cardgitLog",
+    "git status": "cardgitStatus",
     "git fetch": "cardgitFetch",
     "git pull": "cardgitPull",
     "git push": "cardgitPush",
@@ -60,10 +81,19 @@ selectTema.addEventListener("change", () => {
     "git reset": "cardgitReset",
     "generar ssh (windows)": "cardgitSSHW",
     "generar ssh (linux)": "cardgitSSHL",
+    "git config": "cardgitConfig",
+    "git remote add": "cardgitRemoteAdd",
+    "git remote remove": "cardgitRemoteRemove",
+    "git rebase": "cardgitRebase",
+    "git revert": "cardgitRevert",
+    "git diff": "cardgitDiff",
+    "git stash": "cardgitStash",
+    "git commit": "cardgitCommit",
+    "git add": "cardgitAdd",
     "welcome":"welcome",
   };
 
-  selectedOption = gitOptions[selectedOption] || "welcome";
+  selectedOption = gitOptions[selectedOption] || "git";
   muestraCard(selectedOption);
 });
 
@@ -83,4 +113,26 @@ function muestraCard(select) {
       element.setAttribute("hidden", true);
     }
   });
+}
+
+
+function copyCode(elementId, button) {
+  var code = document.getElementById(elementId);
+  if (code) {
+      var range = document.createRange();
+      window.getSelection().removeAllRanges();
+      range.selectNode(code);
+      window.getSelection().addRange(range);
+      navigator.clipboard.writeText(code.innerText || code.textContent)
+          .then(() => {
+              // Cambia el ícono a '✅' cuando la copia es exitosa
+              button.textContent = '✅';
+          })
+          .catch(err => {
+              console.error('Error al copiar:', err);
+          });
+      window.getSelection().removeAllRanges();
+  } else {
+      console.error('Elemento no encontrado:', elementId);
+  }
 }
